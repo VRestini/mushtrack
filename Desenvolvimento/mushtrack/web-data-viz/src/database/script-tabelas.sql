aa5cb71fdf9a7f771092ffd44a305f1cc5d585f2
VRestini <vitor.restini@sptech.school>
2025-05-18 21:06:30 -0300

    :recycle: Refactor: Nova estruturação:  script e mlr do banco de dados; organização de diretórios.

--- Desenvolvimento/mushtrack/web-data-viz/src/database/script-tabelas.sql
+++ Desenvolvimento/mushtrack/web-data-viz/src/database/script-tabelas.sql
@@ -23,42 +23,44 @@ use MushTrack;
  
 create table cogumelo (
 	id int primary key auto_increment,
-	nome varchar (65) NOT NULL
+	nome varchar (65)
 );
 create table estufa (
 	id int primary key auto_increment,
-	nome varchar (45) NOT NULL,
+	nome varchar (45),
 	cogumelo_id int,
-	empresa_id int NOT NULL,
+	empresa_id int,
 	constraint fkEstufaCogumelo foreign key (cogumelo_id) references cogumelo(id),
     constraint fkEstufaEmpresa foreign key (empresa_id) references empresa(id)
 );
 create table sensor (
 	id int primary key auto_increment,
-	tipo varchar (45) NOT NULL,
-	nome varchar(45) NOT NULL,
-	status varchar (45) NOT NULL,
+	tipo varchar (45),
+	nome varchar(45),
+	status varchar (45),
 	constraint chkStatusSensor check (status in ('Ativo', 'Inativo')),
-	numero_serie varchar(20) NOT NULL, 
-	dt_instalacao datetime NOT NULL,
-	dt_manutencao datetime NOT NULL, 
-	posicao varchar(45) NOT NULL,
-	estufa_id int NOT NULL ,
+	numero_serie varchar(20), 
+	dt_instalacao datetime,
+	dt_manutencao datetime, 
+	posicao varchar(45),
+	estufa_id int,
 	constraint fkSensorEstufa foreign key (estufa_id) references estufa (id)
 );
 create table estagio (
 	id int primary key auto_increment,
 	tipo varchar (45),
+	cogumelo_id int,
+	constraint fkEstagioCogumelo foreign key (cogumelo_id) references cogumelo (id)
 );
 
 create table parametro (
 	id int primary key auto_increment,
-	temp_minima decimal(4,2)NOT NULL,
-	temp_maxima decimal(4,2)NOT NULL,
-	umi_minima decimal(4,2) NOT NULL,
-	umi_maxima decimal(4,2) NOT NULL,
-	cogumelo_id int NOT NULL,
-	estagio_id int NOT NULL,
+	temp_minima decimal(4,2),
+	temp_maxima decimal(4,2),
+	umi_minima decimal(4,2),
+	umi_maxima decimal(4,2),
+	cogumelo_id int,
+	estagio_id int,
 	constraint fkParametroCogumelo foreign key (cogumelo_id) references cogumelo(id),
 	constraint fkParametroEstagio foreign key (estagio_id) references estagio(id) 
 );
@@ -66,9 +68,9 @@ create table parametro (
 create table dados(
 	id int primary key auto_Increment, 
 	data_captura datetime default current_timestamp, 
-	umidade float NOT NULL, 
-	temperatura float NOT NULL, 
-	sensor_id int NOT NULL, 
+	umidade float, 
+	temperatura float, 
+	sensor_id int, 
 	constraint fkHistoricoSensor foreign key (sensor_id) references sensor (id)
 );
 
