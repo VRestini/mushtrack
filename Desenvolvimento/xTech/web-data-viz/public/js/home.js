0fcd9c4165b47520e8b72189cf968c5f9e9a1441
VRestini <vitor.restini@sptech.school>
2025-06-10 14:39:28 -0300

    Feature: Novas telas para cada tipo de cogumelo.

--- Desenvolvimento/xTech/web-data-viz/public/js/home.js
+++ Desenvolvimento/xTech/web-data-viz/public/js/home.js
@@ -3,7 +3,7 @@ window.onload = function () {
 }
 var listaEstufas = []
 function buscarTodasEstufas() {
-    id_empresa = 1//sessionStorage.ID_EMPRESA
+    id_empresa = sessionStorage.ID_EMPRESA
     fetch("/estufa/buscar", {
         method: "POST",
         headers: {
@@ -17,14 +17,14 @@ function buscarTodasEstufas() {
             response.json().then(function (response) {
                 for (var i = 0; i < response.length; i++) {
                     listaEstufas.push(response[i])
-                    conteudo_cogumelos_container.innerHTML += `<div class="Cogumelo">
+                    /*conteudo_cogumelos_container.innerHTML += `<div class="Cogumelo">
               <a href="estufas.html">
                 <div class="conteudo">
                   <h2>Shimeji</h2>
                   <img src="img/shimeji_card.png" alt="Shimeji">
                 </div>
               </a>
-            </div>`
+            </div>`*/
                 }
             })
         }
